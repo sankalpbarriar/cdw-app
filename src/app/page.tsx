c272e5385d12b8c921f773b84847876f2bd5db4e
sankalpbarriar <114381745+sankalpbarriar@users.noreply.github.com>
2025-04-06 18:18:17 +0530

    prisma & postgress setup and wrote DB schemas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="text-3xl font-bold tracking-wide p-2 m-3 bg-grey-500 text-center max-w-3xl">CDW is cooking ..</div>
---
+++ src/lib/prisma.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+
+//creating a prisma that will be used to query the database all over the project
+const globalForPrisma = global as unknown as {prisma : PrismaClient};
+
+function makeClient(){
+    return new PrismaClient({log :['error','info','warn']})
+}
+
+export const prisma = globalForPrisma.prisma || makeClient();
+
+if(process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;  

f3e09884766b8255fccd298ad241aa041df796f1
sankalpbarriar <114381745+sankalpbarriar@users.noreply.github.com>
2025-07-01 01:06:59 +0530

    ts ckecks

--- src/components/classified/drag-and-drop-context.tsx
+++ src/components/classified/drag-and-drop-context.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { ClassifiedImages } from "@/config/types";
-import { useCallback, useMemo, useState } from "react";
+import { type JSX,useCallback, useMemo, useState } from "react";
 type ClassifiedImage = ClassifiedImages[number];
 import { type Active, DndContext, type DragEndEvent, KeyboardSensor, PointerSensor, useSensor, useSensors, closestCenter } from "@dnd-kit/core"
-import { arrayMove, sortableKeyboardCoordinates, SortableContext, rectSortingStrategy } from "@dnd-kit/sortable";
+import { arrayMove, sortableKeyboardCoordinates, SortableContext} from "@dnd-kit/sortable";
 import { SortableOverlay } from "./sortable-overlay";
 
 interface DragAndDropContextProps {

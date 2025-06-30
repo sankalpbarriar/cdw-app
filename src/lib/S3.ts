e3f3983edaadbead7287ba1f9c92c161ff9b8171
sankalpbarriar <114381745+sankalpbarriar@users.noreply.github.com>
2025-07-01 01:23:50 +0530

    ts fixes

--- src/lib/S3.ts
+++ src/lib/S3.ts
@@ -1,47 +1,47 @@
 import { env } from "@/env";
-import { Credentials } from "aws-sdk/lib/core";
 import {
-  PutObjectCommand,
-  type PutObjectCommandInput,
-  S3Client,
+	PutObjectCommand,
+	type PutObjectCommandInput,
+	S3Client,
 } from "@aws-sdk/client-s3";
-
-require('aws-sdk/lib/maintenance_mode_message').suppress = true;
+import { Credentials } from "aws-sdk/lib/core";
 
 const credentials = new Credentials({
-  accessKeyId: env.S3_BUCKET_ACCESS_KEY,
-  secretAccessKey: env.S3_BUCKET_SECRET_KEY,
+	accessKeyId: env.S3_BUCKET_ACCESS_KEY,
+	secretAccessKey: env.S3_BUCKET_SECRET_KEY,
 });
 
 export const s3 = new S3Client({
-  region: env.NEXT_PUBLIC_S3_BUCKET_REGION,
-  credentials,
+	region: env.NEXT_PUBLIC_S3_BUCKET_REGION,
+	credentials,
 });
 
 interface UploadToS3Args {
-  bucketName: string;
-  path: string;
-  file: Buffer;
-  mimetype: string;
+	bucketName: string;
+	path: string;
+	file: Buffer;
+	mimetype: string;
 }
-export async function uploadToS3({
-  bucketName,
-  file,
-  path,
-  mimetype,
-}: UploadToS3Args) {
-  const params = {
-    Bucket: bucketName,
-    Key: path,
-    Body: file,
-    ContentType: mimetype,
-    CacheControl: "no-store",
-  } satisfies PutObjectCommandInput;
 
-  try {
-    const command = new PutObjectCommand(params);
-    return s3.send(command);
-  } catch (error:unknown) {
-    throw new Error(`Failed to upload file : ${path} to S3: ${error}`);
-  }
+export async function uploadToS3({
+	bucketName,
+	file,
+	path,
+	mimetype,
+}: UploadToS3Args) {
+	const params = {
+		Bucket: bucketName,
+		Key: path,
+		Body: file,
+		ContentType: mimetype,
+		CacheControl: "no-store",
+	} satisfies PutObjectCommandInput;
+
+	try {
+		const command = new PutObjectCommand(params);
+		return s3.send(command);
+	} catch (error: unknown) {
+		console.log(error);
+		throw new Error(`Failed to upload file: ${path}. Error: ${error}`);
+	}
 }

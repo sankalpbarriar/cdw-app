f7269a449f844d38adab6926c19b34150f451def
sankalpbarriar <114381745+sankalpbarriar@users.noreply.github.com>
2025-08-14 14:22:04 +0530

    revert to previous version of tailwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,5 @@
-// postcss.config.mjs
-export default {
-  plugins: [
-    "@tailwindcss/postcss",
-    "autoprefixer"
-  ],
+const config = {
+  plugins: ["@tailwindcss/postcss"],
 };
+
+export default config;

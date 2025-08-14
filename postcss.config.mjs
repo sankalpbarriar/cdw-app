ad5109202070e6bb02e935eab7346270ce11ade2
sankalpbarriar <114381745+sankalpbarriar@users.noreply.github.com>
2025-08-14 13:42:40 +0530

    tailwind fixes

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,7 @@
-const config = {
+// postcss.config.js
+export default {
   plugins: {
-    tailwindcss: {},
+    "@tailwindcss/postcss": {},
     autoprefixer: {},
   },
 };
-
-export default config;
